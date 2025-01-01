cfe9d8774429a34874ce1fd8c9f6a5ee0b58727d
Ritamsundarsandhaki <lttritam@gmail.com>
2025-01-01 15:47:19 +0530

    update the js

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ function storeData(username, email, password) {
     username: username,
     email: email,
     password: password,
-    appdata: ["null"],
+    appdata: [],
   };
   //   -----------------------------------------------set the data push ---------------------------
   UserDB.push(userdata);
@@ -48,6 +48,70 @@ for(let i=0;i<UserDB.length;i++){
 return index;
 }
 
+// --------------------------------------------------removing item in the array--------------------------------------------
+
+
+
+
+
+
+function fechtheData(finder){
+  const UserDB = JSON.parse(localStorage.getItem("UserDB"))
+  const databox = document.getElementById('databox');
+  databox.innerHTML=``;
+  let i =0;
+  UserDB[finder].appdata.forEach(element => {
+    let div = document.createElement('div');
+      div.innerHTML =`
+    <img src=${`https://img.logo.dev/${element.appname}.com?token=pk_Z0RzmGkIT_mBASPKZFYsBg`} alt="">
+     <p >${element.appname}</p>
+            <br>
+            <p>${element.appusername}</p>
+            <br>
+            <p>${element.apppassword}</p>
+            <br>
+            <button onclick="removeItem(${finder},${i})">remove</button>
+            `
+            databox.appendChild(div)
+            i++;
+  });
+}
+
+
+
+
+function removeItem(userindex,appindex) {
+  const UserDB = JSON.parse(localStorage.getItem("UserDB"))
+  console.log(UserDB[userindex].appdata)
+  console.log(appindex)
+  if (appindex !== -1) {
+    UserDB[userindex].appdata.splice(appindex, 1);
+  }
+console.log("Updated Array: ", UserDB[userindex].appdata);
+localStorage.setItem("UserDB", JSON.stringify(UserDB));
+fechtheData(userindex)
+}
+function add(index){
+alert("call the add");
+console.log(index)
+const appname = document.getElementById('appname').value.trim();
+const appusername = document.getElementById('appusername').value.trim();
+const apppassword = document.getElementById('apppassword').value.trim();
+if(appname==''||appusername==''||apppassword==''){
+  alert("enter the data");
+  return;
+}
+const UserDB = JSON.parse(localStorage.getItem("UserDB")) || [];
+console.log(UserDB[index].appdata);
+let disk = {
+  appname:appname,
+  appusername:appusername,
+  apppassword:apppassword
+}
+UserDB[index].appdata.push(disk)
+localStorage.setItem("UserDB", JSON.stringify(UserDB));
+fechtheData(index);
+}
 document.getElementById("Signup").addEventListener("click", () => {
   let username = document.getElementById("Sname").value.trim();
   let email = document.getElementById("Semail").value.trim();
@@ -64,7 +128,8 @@ document.getElementById("Signup").addEventListener("click", () => {
      alert("user Alardy Exsist");
      return;
   }
-//   F ? (storeData(username, email, pass),alert("signup sucessful")) : alert("User Exist");
+  storeData(username, email, pass);
+  alert("signup sucessful")
 
   //-------------------------------------------empting the input box-----------------------------------
   document.getElementById("Sname").value = "";
@@ -93,6 +158,19 @@ document.getElementById("login").addEventListener("click", () => {
   const  main = document.getElementById('main');
     athentication.style.display='none';
     main.style.display='';
+    const btn = document.getElementById('btn');
+    const databox = document.getElementById('databox');
+    const UserDB = JSON.parse(localStorage.getItem("UserDB")) || [];
+    btn.innerHTML=` <button id="finalsubbmit"  onclick="add(${finder})">Submit</button>`
+
+    UserDB[finder].appdata
+    console.log( UserDB[finder].appdata.length);
+    if(UserDB[finder].appdata.length==0){
+      databox.innerHTML=" <h1>No Data Found</h1>"
+    }else{
+      fechtheData(finder);
+    }
+    
  }
 
 //   F ? alert("wrong username or password") : alert("userlogin");
@@ -101,8 +179,3 @@ document.getElementById("login").addEventListener("click", () => {
   document.getElementById("Lpassword").value = "";
 });
 
-document.getElementById("finalsubbmit").addEventListener("click",()=>{
-    // let aapname = document.getElementById("aapname").value.trim();
-    // let appusername= document.getElementById("appusername").value.trim();
-    console.log("btn click")
-})
